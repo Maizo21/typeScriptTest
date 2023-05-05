40edd29a79ff4aa602aab5484a8a5532a843eb16
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2023-05-05 19:00:09 -0400

    Creacion de archivos de tipos de datos

---
+++ src/03-typing.ts
@@ -0,0 +1,8 @@
+let myProductName = "Phone";
+let myProductPrice = 1000;
+
+myProductName.toLowerCase();
+myProductPrice.toFixed(2);
+
+// myProductPrice.toLowerCase(); // Error: Property 'toLowerCase' does not exist on type 'number'.
+// myProductName.toFixed(2); // Error: Property 'toFixed' does not exist on type 'string'.
